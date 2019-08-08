7c06c981264e76fd9f049d97e63f3c1474d3833e
pascalmoser <pascalmoser@gmail.com>
2019-08-08 10:56:34 +0200

    add asset meta extractor

---
+++ src/DsTrinityDataBundle/Resource/FieldTransformer/Asset/AssetMetaExtractor.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace DsTrinityDataBundle\Resource\FieldTransformer\Asset;
+
+use DynamicSearchBundle\Resource\Container\ResourceContainerInterface;
+use DynamicSearchBundle\Resource\FieldTransformerInterface;
+use Pimcore\Model\Asset;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AssetMetaExtractor implements FieldTransformerInterface
+{
+    /**
+     * @var array
+     */
+    protected $options;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['name', 'locale']);
+        $resolver->setAllowedTypes('name', ['string']);
+        $resolver->setAllowedTypes('locale', ['string', 'null']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**+
+     * @param string                     $dispatchTransformerName
+     * @param ResourceContainerInterface $resourceContainer
+     *
+     * @return int|mixed|null
+     * @throws \Exception
+     */
+    public function transformData(string $dispatchTransformerName, ResourceContainerInterface $resourceContainer)
+    {
+        $asset = $resourceContainer->getResource();
+        if (!$asset instanceof Asset) {
+            return null;
+        }
+
+        if(!$asset->getHasMetaData()) {
+            return null;
+        }
+
+        $metaData = $asset->getMetadata($this->options['name']);
+
+        return $metaData;
+
+    }
+}
