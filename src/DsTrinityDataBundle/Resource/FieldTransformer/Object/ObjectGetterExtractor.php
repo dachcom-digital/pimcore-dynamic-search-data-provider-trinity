c95fb270b825ab573e17e52d5cae3829afc678be
Ben Walch <bwalch@dachcom.ch>
2022-11-15 11:56:10 +0100

    more flexibility for object getter extractor (#24)

--- src/DsTrinityDataBundle/Resource/FieldTransformer/Object/ObjectGetterExtractor.php
+++ src/DsTrinityDataBundle/Resource/FieldTransformer/Object/ObjectGetterExtractor.php
@@ -12,14 +12,16 @@ class ObjectGetterExtractor implements FieldTransformerInterface
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setRequired(['method', 'arguments', 'clean_string']);
+        $resolver->setRequired(['method', 'arguments', 'clean_string', 'format']);
         $resolver->setAllowedTypes('method', ['string']);
         $resolver->setAllowedTypes('arguments', ['array']);
         $resolver->setAllowedTypes('clean_string', ['boolean']);
+        $resolver->setAllowedTypes('format', ['null', 'string']);
         $resolver->setDefaults([
             'method'       => 'id',
             'clean_string' => true,
-            'arguments'    => []
+            'arguments'    => [],
+            'format'       => null
         ]);
     }
 
@@ -28,7 +30,7 @@ class ObjectGetterExtractor implements FieldTransformerInterface
         $this->options = $options;
     }
 
-    public function transformData(string $dispatchTransformerName, ResourceContainerInterface $resourceContainer): ?string
+    public function transformData(string $dispatchTransformerName, ResourceContainerInterface $resourceContainer): null|bool|int|float|string|array
     {
         if (!$resourceContainer->hasAttribute('type')) {
             return null;
@@ -40,12 +42,30 @@ class ObjectGetterExtractor implements FieldTransformerInterface
         }
 
         $value = call_user_func_array([$data, $this->options['method']], $this->options['arguments']);
-        if (!is_string($value)) {
-            return null;
+        
+        if ($value instanceof \DateTimeInterface) {
+            return $this->options['format'] ? $value->format($this->options['format']) : $value->getTimestamp();
         }
-
-        if ($this->options['clean_string'] === true) {
-            return trim(preg_replace('/\s+/', ' ', strip_tags($value)));
+        
+        if (is_bool($value)) {
+            return $value;
+        }
+        
+        if (is_numeric($value)) {
+            $value = (float) $value;
+            if (floor($value) === $value) {
+                return (int) $value;
+            }
+            return $value;
+        }
+        
+        if (is_string($value)) {
+            if ($this->options['format'] === 'nl2br') {
+                return nl2br($value);
+            }
+            if ($this->options['clean_string'] === true) {
+                return trim(preg_replace('/\s+/', ' ', strip_tags($value)));
+            }
         }
 
         return $value;
